61c8756996415103505ed12e787ca0afc326178f
Nicholas H <NicholasH7@hotmail.com>
2023-05-02 15:33:10 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import fitness from './fitness.png';
 
 function App() {
-  return (
+  return (  
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<img src={fitness} className='sidebar' alt="logo" width='500px' height='685px' />
+
+
+<div className="wrapper">
+         
+<header className="h3">
+<h3>
+Are you looking for someone to help motivate you? <br/>
+ Hold you accountable? Don't wait to sign up and join our amazing community! </h3>
+
+</header>
+
+
+
+
+       
+<form action='get'>
+ <div className="formContainer">
+
+
+
+<div className="contentContainer">
+
+<div className="space"><label for='firstName'>FIRST NAME</label>
+<input type='text' name='firstName' placeholder='Enter your name...' />
+</div>
+
+<div className="space">
+<label for='lastName'>LAST NAME</label>
+<input type='text' name='lastName' placeholder='Enter your name...' />
+</div>
+</div>
+
+
+
+
+<div className="contentContainer">
+
+<div className="space"><label for='email'>E-MAIL</label>
+<input type='email' name='email' placeholder='Enter your E-mail...' />
+</div>
+
+<div className="space">
+<label for='tel'>PHONE NUMBER</label>
+<input type='tel' name='tel' placeholder='Enter your phone number...' />
+</div>
+</div>
+
+
+
+
+
+
+
+<div className="contentContainer">
+
+<div className="space"><label for='pass'>PASSWORD</label>
+<input type='password' name='pass' placeholder='Enter password' />
+</div>
+
+<div className="space">
+<label for='confirm'>CONFIRM PASSWORD</label>
+<input type='password' name='confirm' placeholder='Confirm password' />
+</div>
+</div>
+
+</div>
+</form>
+
+        
+
+<div className="footer">
+
+
+<div className="buttonSection">
+
+ <button className='button'> Create Account </button>
+</div>
+
+<div className="haveAccount">
+  
+  <p className='account'> Already have an account? Log In</p>
+</div>
+
+</div>
     </div>
+    </div>
+    
   );
 }
 
